7dc78ceb850ef9e874db00ae24fc033448f9a6bb
slepinay@guardiaschool.fr <slepinay@guardiaschool.fr>
2025-04-29 23:18:10 +0000

    Edited Components.FormButtons, Pages.Session.Admin.Dashboard_admin, Pages.Session.Customer.Cart, Pages.Session.Customer.Cart.SouCart and 3 more; Deleted Pages.Session.Customer.Orders.OrderDetails and Pages.Session.Customer.Orders.OrderDetails.ProductItem

--- client_code/Pages/Session/Customer/Orders/__init__.py
+++ client_code/Pages/Session/Customer/Orders/__init__.py
@@ -5,7 +5,6 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 from .... import state
-from .OrderDetails import OrderDetails
 
 
 class Orders(OrdersTemplate):
