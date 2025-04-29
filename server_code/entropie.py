2b3869da5440e6536786d5cdc056062bc22f92f3
7exiu <slepinay@guardiaschool.fr>
2025-04-29 21:50:03 +0100

    Écrase les anciennes versions avec les nouvelles modifications

--- server_code/entropie.py
+++ server_code/entropie.py
@@ -4,6 +4,7 @@ from anvil.tables import app_tables
 import anvil.server
 import math
 import string
+import re
 
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
@@ -26,8 +27,26 @@ def calculer_entropie(mot_de_passe):
         mot_de_passe (str): Le mot de passe à analyser
     
     Returns:
-        tuple: (entropie en bits, taille de l'alphabet utilisé)
+        tuple: (entropie en bits, taille de l'alphabet utilisé, redon)
     """
+    # Vérifications de base
+    if len(mot_de_passe) < 12:
+        return 0, 0, 0  # Mot de passe trop court
+    
+    # Vérification des caractères requis
+    has_upper = bool(re.search(r'[A-Z]', mot_de_passe))
+    has_lower = bool(re.search(r'[a-z]', mot_de_passe))
+    has_digit = bool(re.search(r'\d', mot_de_passe))
+    has_special = bool(re.search(r'[!@#$%^&*(),.?":{}|<>]', mot_de_passe))
+    
+    if not (has_upper and has_lower and has_digit and has_special):
+        return 0, 0, 0  # Manque des types de caractères requis
+    
+    # Vérification des séquences répétitives
+    if re.search(r'(.)\1{2,}', mot_de_passe):
+        return 0, 0, 0  # Trop de répétitions
+    
+    # Calcul de l'entropie
     minuscules = set(string.ascii_lowercase)
     majuscules = set(string.ascii_uppercase)
     chiffres = set(string.digits)
@@ -41,25 +60,36 @@ def calculer_entropie(mot_de_passe):
     if any(c in chiffres for c in mot_de_passe):
         taille_alphabet += 10
     if any(c in symboles for c in mot_de_passe):
-        taille_alphabet += 32  # Il y a 32 caractères de ponctuation
+        taille_alphabet += 32
     
     entropie = len(mot_de_passe) * math.log2(taille_alphabet)
     taille = len(mot_de_passe)
     redon = (taille * math.log2(taille_alphabet)) - entropie
 
-    return entropie, taille_alphabet , redon
+    return entropie, taille_alphabet, redon
     
-@anvil.server.callable  
+@anvil.server.callable
 def evaluer_securite(entropie):
-      if entropie < 28:
+    """
+    Évalue la sécurité d'un mot de passe basée sur son entropie.
+    
+    Args:
+        entropie (float): L'entropie du mot de passe en bits
+        
+    Returns:
+        str: Niveau de sécurité
+    """
+    if entropie == 0:
+        return "Invalide"
+    elif entropie < 60:
         return "Très faible"
-      elif entropie < 36:
+    elif entropie < 80:
         return "Faible"
-      elif entropie < 60:
+    elif entropie < 100:
         return "Moyenne"
-      elif entropie < 128:
+    elif entropie < 120:
         return "Forte"
-      else:
+    else:
         return "Très forte"
 
   
